ddff7e5421db3e0282cd7fb622c96d33b54a1c3f
Hoque <hoque.rashedul@iisgalvanimi.edu.it>
2019-05-16 12:48:23 +0000

    ciao

--- Mono-rent/src/app/app.component.ts
+++ Mono-rent/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { send } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mono-rent';
+  utenti : Array<string> = [];
+
+
+
+
+
+  send(user:HTMLInputElement) : boolean{
+ this.utenti.push(user.value);
+ return false;
+}
+
+
+
 }
